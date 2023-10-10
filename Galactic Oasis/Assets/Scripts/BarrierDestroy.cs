693d8194767d068e99b08d59ccf87df070f5274e
LukeDeMaria <137795304+LukeDeMaria@users.noreply.github.com>
2023-10-10 17:11:03 -0400

    Rocket Parts and Test Setup

--- Galactic Oasis/Assets/Scripts/BarrierDestroy.cs
+++ Galactic Oasis/Assets/Scripts/BarrierDestroy.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BarrierDestroy : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public int rocketPartsNeeded = 5;
+    public int rocketPartsHad = 0;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(rocketPartsHad == rocketPartsNeeded)
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+   public void UpdateParts()
+    {
+        rocketPartsHad++; 
     }
 }
---
+++ Galactic Oasis/Assets/Scripts/RocketPartFunc.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketPartFunc : MonoBehaviour
+{
+
+    private BarrierDestroy barrierDestroy; 
+
+    void Start()
+    {
+        barrierDestroy = GameObject.Find("BarrierWall").GetComponent<BarrierDestroy>(); 
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        barrierDestroy.UpdateParts();
+        Destroy(gameObject);
+    }
+}
