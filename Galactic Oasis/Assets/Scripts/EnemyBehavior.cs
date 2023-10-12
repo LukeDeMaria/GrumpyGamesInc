ab4ad0226540bd53a3273fb9f8af1dff7b93d715
LukeDeMaria <137795304+LukeDeMaria@users.noreply.github.com>
2023-10-12 08:21:22 -0400

    Enemy Setup 2

--- Galactic Oasis/Assets/Scripts/EnemyBehavior.cs
+++ Galactic Oasis/Assets/Scripts/EnemyBehavior.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    public int speed = 5; 
+    public float speed;
+    public int health;
+    private Rigidbody enemyRB;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("PlayerTemp");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRB.AddForce(lookDirection * speed);
     }
 }
