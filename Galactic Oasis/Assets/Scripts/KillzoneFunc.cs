eb10f4e47c4fd4b69245e67faeafbac48fb43b63
jacob0503 <idgyrules@gmail.com>
2023-10-23 08:22:41 -0400

    Working on killzone and health

--- Galactic Oasis/Assets/Scripts/KillzoneFunc.cs
+++ Galactic Oasis/Assets/Scripts/KillzoneFunc.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
+public class KillzoneFunc : MonoBehaviour
 {
-    public int health;
-
+    public ThirdPersonMovement playerHealth = new ThirdPersonMovement;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +15,10 @@ public class PlayerHealth : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        playerHealth.health = 0;
     }
 }
