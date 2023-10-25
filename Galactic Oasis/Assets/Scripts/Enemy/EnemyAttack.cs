c3c14854735303a65cf5e3ce111a17493157e83b
LukeDeMaria <137795304+LukeDeMaria@users.noreply.github.com>
2023-10-25 09:08:08 -0400

    Sword Animation

---
+++ Galactic Oasis/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public GameObject sword; 
+    public float attackMinTime = 1.2f;
+    public float attackMaxTime = 3.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
