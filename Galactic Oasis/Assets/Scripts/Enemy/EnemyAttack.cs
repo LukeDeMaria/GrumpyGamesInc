93c04328e5a35f9b009adc01fd31e24df20cb757
LukeDeMaria <137795304+LukeDeMaria@users.noreply.github.com>
2023-10-26 08:04:36 -0400

    Enemy

--- Galactic Oasis/Assets/Scripts/Enemy/EnemyAttack.cs
+++ Galactic Oasis/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     public GameObject sword; 
-    public float attackMinTime = 1.2f;
-    public float attackMaxTime = 3.0f; 
+    public float attackMinTime = 1.5f;
+    public float attackMaxTime = 5.5f; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("SwordAttack", attackMaxTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SwordAttack()
+    {
+        Animator anim = sword.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+        float attackTime = Random.Range(attackMinTime, attackMaxTime);
+        Invoke("SwordAttack", attackTime);
     }
 }
