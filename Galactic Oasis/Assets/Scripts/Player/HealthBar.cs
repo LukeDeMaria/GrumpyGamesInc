dac1803194aa4009d3e66a998b95bbc799bf1a87
jacob0503 <idgyrules@gmail.com>
2023-11-10 08:05:52 -0500

    Wor,in ibg on healthi jkbar

--- Galactic Oasis/Assets/Scripts/Player/HealthBar.cs
+++ Galactic Oasis/Assets/Scripts/Player/HealthBar.cs
@@ -7,11 +7,14 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
     public Gradient gradient;
+    public Image fill;
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
