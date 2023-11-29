e664b768b2ddf66bf4ec244e3920164d815c3ca7
LukeDeMaria <137795304+LukeDeMaria@users.noreply.github.com>
2023-11-29 08:01:15 -0500

    LaserMovement

--- Galactic Oasis/Assets/Scripts/Tower/Tower_Laser_Funct.cs
+++ Galactic Oasis/Assets/Scripts/Tower/Tower_Laser_Funct.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Tower_Laser_Funct : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Tower_Laser_Funct : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
     }
 }
