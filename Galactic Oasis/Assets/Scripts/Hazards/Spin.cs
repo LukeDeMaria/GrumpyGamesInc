a6f43b819e4dfd5ab9d8ec5e2237ab71c8a8d71b
jacob0503 <idgyrules@gmail.com>
2024-01-31 09:08:09 -0500

    Made spinning rocks

--- Galactic Oasis/Assets/Scripts/Hazards/Spin.cs
+++ Galactic Oasis/Assets/Scripts/Hazards/Spin.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gear : MonoBehaviour
+public class Spin : MonoBehaviour
 {
     public float spinSpeed;
     // Start is called before the first frame update
