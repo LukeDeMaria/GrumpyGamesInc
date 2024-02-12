a55ad20d7cde0f11b593607fb5fe3cd3f4fccd7f
LukeDeMaria <137795304+LukeDeMaria@users.noreply.github.com>
2024-02-12 08:35:20 -0500

    Mushroom Boss

--- Galactic Oasis/Assets/Scripts/Enemy/MushroomBoss/AOEExpandMush.cs
+++ Galactic Oasis/Assets/Scripts/Enemy/MushroomBoss/AOEExpandMush.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class AOEExpandMush : MonoBehaviour
 {
     public Vector3 expandRate = new Vector3 (1, 1, 1);
+    public float expandTime = 1.0f;
+    public float secondsTillDestroy = 8.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Expand();
+        Destroy(gameObject, secondsTillDestroy);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Expand()
+    {
+        transform.localScale += expandRate;
+        Invoke("Expand", expandTime);
     }
 }
