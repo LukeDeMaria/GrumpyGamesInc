436d06c4512ad9daf15d925360084adb1e054360
jacob0503 <idgyrules@gmail.com>
2024-03-15 08:52:33 -0400

    s

--- Galactic Oasis/Assets/Scripts/Enemy/EyeSpinAttack.cs
+++ Galactic Oasis/Assets/Scripts/Enemy/EyeSpinAttack.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EyeSpin : MonoBehaviour
+public class EyeSpinAttack : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject eye;
+    public Transform attackPoint;
+    public ThirdPersonMovement tpm;
+    public float attackMinTime;
+    public float attackMaxTime;
+    public float attackRange = 0.5f;
+    public LayerMask playerLayer;
+    public LookAtPlayer lookAtPlayer;
+    public UnityEngine.AI.NavMeshAgent agent;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent.updateRotation = false;
+        Invoke("SpinAttack", attackMaxTime);
+        lookAtPlayer = GetComponent<LookAtPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpinAttack()
+    {
+        lookAtPlayer.canFollow = false;
+        Quaternion newRotation = eye.transform.rotation;
+        eye.transform.rotation = Quaternion.RotateTowards(eye.transform.rotation, newRotation, Time.deltaTime * 180);
+
+        //brackeys melee combat tutorial 
+        EnemySpinFunct();
+
+        float attackTime = Random.Range(attackMinTime, attackMaxTime);
+        lookAtPlayer.canFollow = true;
+        Invoke("SpinAttack", attackTime);
+    }
+
+    void EnemySpinFunct()
+    {
+        Collider[] hitPlayer = Physics.OverlapSphere(attackPoint.position, attackRange, playerLayer);
+
+        foreach (Collider player in hitPlayer)
+        {
+            Debug.Log("HIT!");
+            tpm.TakeDamage(2);
+
+        }
     }
 }
--- Galactic Oasis/Assets/Scripts/Enemy/LookAtPlayer.cs
+++ Galactic Oasis/Assets/Scripts/Enemy/LookAtPlayer.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class LookAtPlayer : MonoBehaviour
 {
     public Transform target;
+    public bool canFollow = true;
 
     void Update()
     {
-        if (target != null)
+        if (target != null && canFollow == true)
         {
             transform.LookAt(target);
         }
