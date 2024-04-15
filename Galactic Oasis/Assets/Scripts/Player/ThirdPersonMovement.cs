0a90f264502373da833e9a3586734f5f236b890e
LukeDeMaria <137795304+LukeDeMaria@users.noreply.github.com>
2024-04-15 07:58:32 -0400

    Boletus Sword

--- Galactic Oasis/Assets/Scripts/Player/ThirdPersonMovement.cs
+++ Galactic Oasis/Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -19,12 +19,12 @@ public class ThirdPersonMovement : MonoBehaviour
     public Material invinc, defaultAstro;
 
     Vector3 velocity;
-    [HideInInspector] public GameObject astronautRig, respawnPnt, dashBlue, dashGray;
-    [HideInInspector] public HealthBar healthBar;
-    [HideInInspector] public AudioSource audioSource;
-    [HideInInspector] public CharacterController controller;
+    public GameObject astronautRig, respawnPnt, dashBlue, dashGray;
+    public HealthBar healthBar;
+    public AudioSource audioSource;
+    public CharacterController controller;
     [HideInInspector] public RocketFunc rocket;
-    [HideInInspector] public Transform check, cam;
+    public Transform check, cam;
     [HideInInspector] public float iFrames = 1.0f, damageCooldown = 0, checkDistance = 0.4f, turnSmoothTime = 0.1f, horizontalInput, verticalInput, turnSmoothVelocity;
     public LayerMask groundMask, groundMask2, killzoneMask, hazardMask, mudMask, bouncyLowMask, bouncyMedMask, bouncyHighMask, poisonMask, rocketMask;
     [HideInInspector] public bool hasDashed = false, isGrounded, isGrounded2, touchingKillzone, touchingHazard, touchingHazard2, touchingMud, touchingBouncyLow, touchingBouncyMed, touchingBouncyHigh, touchingRocketPart, touchingPoison, isPoisoned, takingChipDamage;
